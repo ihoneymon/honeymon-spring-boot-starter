c676e3f593dab08cffdd9927ab887103d74bc84f
honeymon <ihoneymon@gmail.com>
2018-01-21 15:07:09 +0900

    File change: README.md -> README.asc

--- antipang-app/src/main/java/io/honeymon/boot/antipang/app/AntiPangApplication.java
+++ antipang-app/src/main/java/io/honeymon/boot/antipang/app/AntiPangApplication.java
@@ -2,11 +2,19 @@ package io.honeymon.boot.antipang.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AntiPangApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AntiPangApplication.class, args);
+		/**
+		 * 메서드 체이닝 지원
+		 */
+//		SpringApplication.run(AntiPangApplication.class, args);
+		
+		SpringApplication app = new SpringApplication(AntiPangApplication.class);
+		//app 구동시 초기구성 가능
+		app.run(args);
 	}
 }
