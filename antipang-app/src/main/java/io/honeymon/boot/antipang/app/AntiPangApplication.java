9475c095033aa4cc18883e1669652c52993ee934
honeymon <ihoneymon@gmail.com>
2018-01-25 23:02:58 +0900

    @Conditional 예제 추가, jojoldu.tistory.com 링크 추가

--- antipang-app/src/main/java/io/honeymon/boot/antipang/app/AntiPangApplication.java
+++ antipang-app/src/main/java/io/honeymon/boot/antipang/app/AntiPangApplication.java
@@ -2,9 +2,10 @@ package io.honeymon.boot.antipang.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {ThymeleafAutoConfiguration.class})
 public class AntiPangApplication {
 
 	public static void main(String[] args) {
